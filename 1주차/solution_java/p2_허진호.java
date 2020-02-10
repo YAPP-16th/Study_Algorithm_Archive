3f09f14af79db2b9746661ac13d5d65790271546
Jinho Huh <jh8579@gmail.com>
2020-02-10 20:00:54 +0900

    Update p2_허진호.java

--- 1주차/solution_java/p2_허진호.java
+++ 1주차/solution_java/p2_허진호.java
@@ -2,27 +2,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class p2_허진호 {
-    public static int solve(Integer num, int total){
-        int temp = 0;
+    public static int solve(Integer num){
+        int count = 0;
 
         if(num == 0){
             return 1;
         }
 
         if(num >= 1){
-            temp += solve(num-1, total+1);
+            count += solve(num-1);
         }
 
         if(num >= 2){
-            temp += solve(num-2, total+1);
+            count += solve(num-2);
         }
 
         if(num >= 3){
-            temp += solve(num-3, total+1);
+            count += solve(num-3);
         }
 
-        return temp;
+        return count;
     }
+    
     public static void main(String []args){
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
@@ -30,12 +31,11 @@ public class p2_허진호 {
         ArrayList<Integer> number = new ArrayList<Integer>();
 
         for(int i=0; i<t; i++){
-            Integer a = scan.nextInt();
-            number.add(a);
+            number.add(scan.nextInt());
         }
 
         for(int i=0; i<t; i++){
-            System.out.println(solve(number.get(i), 0));
+            System.out.println(solve(number.get(i)));
         }
     }
 }
